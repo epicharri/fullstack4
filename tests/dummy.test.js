e66f1c24269bbae8f88d558ebddddb5d35776930
Kähkönen Harri J <harrikah@lx8-523-012.pc.helsinki.fi>
2019-02-18 10:25:33 +0200

    4.3 testaus aloitettu

---
+++ tests/dummy.test.js
@@ -0,0 +1,9 @@
+const listHelper = require('../utils/list_helper')
+
+test('Dummy returns one', () => {
+  const blogs = []
+
+  const result = listHelper.dummy(blogs)
+  expect(result).toBe(1)
+  
+})
