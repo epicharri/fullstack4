42f5d38d2de3696e006ad1f27d1e4a4e6e3c43ee
Kähkönen Harri J <harrikah@lx8-523-012.pc.helsinki.fi>
2019-02-18 11:29:39 +0200

    4.1 - 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,20 @@
-const dummy = (blogs) => {
+const dummy = blogs => {
   return 1
 }
 
+const totalLikes = blogs => {
+  const summa = (total, number) => {
+    return total + number
+  }
+
+  return blogs.map(
+    blog => blog.likes
+  ).reduce(
+    summa, 0
+  )
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
